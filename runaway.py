40f49e064d5d758e9559686ca099de1c128d0b49
matias <m.borquez.n.3i@gmail.com>
2024-04-12 20:50:15 -0400

    ejercicios uwu

---
+++ runaway.py
@@ -0,0 +1,23 @@
+print("Bienvenido al mundo de la programacion !")
+nom = input ("para empezar, ingresa tu nombre ")
+print (f"bienvenido {nom} ")
+
+
+#ejercicio 3
+print("ingresa un valor para 'x' y podras resolver la siguiente ecuacion ")
+x = int(input("(x^2+3*x+1)/4"))
+formula = (x**2+3*x+1)/4
+print ("el resultado de la operacion con el valor {x} es: {formula} ", formula)
+
+#ejercicio 4
+
+nombre = input ("ingrese su nombre! :P ")
+rut = input ("ingrese su rut!  :3 ")
+correo = input ("ingrese su correo electronico uwu ")
+telefono = input("ingrese su numero telefonico ")
+dat= "datos personales"
+print (dat.upper())
+print ("NOMBRE: \t", nombre.upper())
+print ("RUT: \t\t", rut)
+print ("CORREO: \t", correo.upper())
+print ("TELEFONO \t", telefono)
